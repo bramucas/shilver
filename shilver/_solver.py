82154aa24865aa35980db57e45b913e8c9fabf1f
Brais Muñiz <mc.brais@gmail.com>
2023-04-19 12:15:18 +0200

    Adds Solver class for clients and updates gitignore

--- shilver/_solver.py
+++ shilver/_solver.py
@@ -1,4 +1,5 @@
 from clingo import Control
+from collections.abc import Sequence
 
 from ._solution import Solution
 from ._encoder import Encoder
@@ -6,18 +7,28 @@ from .shilver_lp import SOLVER_LP, VIZ_LP
 
 from clingraph.clingo_utils import ClingraphContext
 
-class Solver:
+class Solver(Control):
     def __init__(self):
-        self.control = Control([])
+        super().__init__([])
 
-    def compute_solution(self, input_file) -> Solution:
-        enc = Encoder()
-        self.control.add('base', [], enc.from_file(input_file))
-        self.control.add('base', [], SOLVER_LP)
-        self.control.add('base', [], VIZ_LP)
+    def add_instance_from_file(self, input_file):
+        self.add('base', [], Encoder().from_file(input_file))
+
+    def add_str_facts(self, str_facts):
+        self.add('base', [], str_facts)
+
+    def _get_solutions(self, input_file) -> Sequence[Solution]:
+        self.add('base', [], SOLVER_LP)
+        self.add('base', [], VIZ_LP)
         
-        self.control.ground([('base',[])], context=ClingraphContext())
+        self.ground([('base',[])], context=ClingraphContext())
+        
+        solutions = []
+        self.solve(on_model=lambda m: solutions.append(Solution.from_model(m)))
+        return solutions
 
-        s = []
-        self.control.solve(on_model=lambda m: s.append(Solution.from_model(m)))
-        return s.pop()
+    def get_solution(self, input_file=None, asp_str=None) -> Solution:
+        return self._get_solutions(input_file=input_file).pop()
+
+    def _check_individual_solution(self, input_file=None, asp_str=None):
+        return len(self._get_solutions(input_file=input_file)) == 1
